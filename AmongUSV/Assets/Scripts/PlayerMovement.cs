887c7e405123d9be82d29329b70a093dc4e5dfcf
Calin201 <xinzhao2012@yahoo.ro>
2023-03-12 13:47:29 +0200

    comit

---
+++ AmongUSV/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSensitivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
+    public bool invertLook;
+    public float moveSpeed = 5f, runSpeed = 8f;
+    private float activeMoveSpeed;
+
+    private Vector3 moveDir, movement;
+
+    public CharacterController charCon;
+
+    private Camera cam;
+
+    public float jumpForce = 12f,gravityMod = 2.5f;
+
+    public Transform groundCheckPoint;
+    private bool isGrounded;
+    public LayerMask groundLayers;
+
+    public GameObject bulletImpact;
+    public float timeBetweenShots = .1f;
+    public float shotCounter;
+
+    public float maxHeat = 10f,heatPerShot=1f,coolRate=4f,overheatCoolRate=5f;
+    private float heatCounter;
+    private bool overHeated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cam = Camera.main;
+
+        UIController.instance.weaponTempSlider.maxValue = maxHeat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseInput = new Vector2( Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60f, 60f);
+
+        viewPoint.rotation = Quaternion.Euler(invertLook ? verticalRotStore:-verticalRotStore , viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z) ;
+
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        activeMoveSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        float yVel = movement.y;
+
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+        movement.y= charCon.isGrounded  ? 0f: yVel;
+
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .25f, groundLayers);
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            movement.y = jumpForce;
+        }
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+   
+        charCon.Move( movement * Time.deltaTime);
+
+
+        if (!overHeated)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                shotCounter -= Time.deltaTime;
+
+                if (shotCounter <= 0)
+                {
+                    Shoot();
+
+                }
+            }
+            heatCounter -= coolRate * Time.deltaTime;
+        }
+        else
+        {
+            heatCounter -= overheatCoolRate * Time.deltaTime;
+            if(heatCounter <= 0)
+            {
+                
+                overHeated = false;
+                UIController.instance.overheatedMessage.gameObject.SetActive(false);
+            }
+        }
+        if (heatCounter < 0)
+        {
+            heatCounter = 0f;
+        }
+        UIController.instance.weaponTempSlider.value = heatCounter;
+
+
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        } else if (Cursor.lockState == CursorLockMode.None)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+        ray.origin = cam.transform.position;
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("We hit" + hit.collider.gameObject.name);
+            GameObject bulletImpactObject = Instantiate(bulletImpact, hit.point +(hit.normal*.002f),Quaternion.LookRotation(hit.normal,Vector3.up));
+
+            Destroy(bulletImpactObject, 10f);
+        }
+
+        shotCounter = timeBetweenShots;
+
+
+        heatCounter += heatPerShot;
+        if(heatCounter >= maxHeat)
+        {
+            heatCounter = maxHeat;
+            overHeated = true;
+
+            UIController.instance.overheatedMessage.gameObject.SetActive(true);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
+    }
+}
